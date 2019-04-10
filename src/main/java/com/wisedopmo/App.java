946a7e3a1e1e20ee4d23189570c1f6419000040c
liujn@mobws.com <Ljn456852qwe>
2019-04-10 22:19:27 +0800

    初始化项目【慧都PMO系统】

---
+++ src/main/java/com/wisedopmo/App.java
@@ -0,0 +1,13 @@
+package com.wisedopmo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
