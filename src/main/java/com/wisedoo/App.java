c9c53462fff80529992165336732a7607da5838b
liujn <liujn@yonyounc.com>
2019-04-10 23:07:37 +0800

    初始化项目POM 以及mybatis

--- src/main/java/com/wisedoo/App.java
+++ src/main/java/com/wisedoo/App.java
@@ -1,13 +1,19 @@
 package com.wisedoo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@MapperScan("com.wisedoo.project.dao")
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
+
+
+
 }
