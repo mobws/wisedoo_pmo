060de019e40657c8c00b03f20c23acf7610dd99a
liujn <liujn@yonyounc.com>
2019-09-29 09:56:19 +0800

    增加项目license申请模块

---
+++ src/main/java/com/wisedoo/project/license/controller/ProLicenseController.java
@@ -0,0 +1,13 @@
+package com.wisedoo.project.license.controller;
+
+/**
+ * @ClassName: ProLicenseController
+ * @Description: TODO
+ * @Auther: liujn
+ * @Date: 2019/9/29 9:55 AM
+ * @Version 1.0
+ */
+public class ProLicenseController {
+
+
+}
